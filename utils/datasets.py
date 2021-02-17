60bd4411bb5a52540f68c77a3e59f6b0631a3fec
Cecilia <cema3961@colorado.edu>
2021-02-17 14:59:08 -0700

    Change cache location

--- utils/datasets.py
+++ utils/datasets.py
@@ -382,7 +382,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Check cache
         self.label_files = img2label_paths(self.img_files, img_suffix, label_suffix)  # labels
-        cache_path = Path(self.label_files[0]).parent.with_suffix('.cache')  # cached labels
+        cache_path = Path(path).with_suffix('.cache')  # cached labels
         if cache_path.is_file():
             cache = torch.load(cache_path)  # load
             if cache['hash'] != get_hash(self.label_files + self.img_files) or 'results' not in cache:  # changed

65fda2a2223be6b1cd09156ef51df0a53cbb426d
Cecilia <cema3961@colorado.edu>
2021-02-23 07:56:25 -0700

    Switch to yacs config

---
+++ data/config_default.py
@@ -0,0 +1,60 @@
+from yacs.config import CNode as CN
+from utils.general import increment_path, check_file
+import torch, os
+from pathlib import Path
+
+_C = CN()
+
+_C.weights = 'yolov5s.pt' #initial weights path
+_C.C = ''  #'model.yaml path')
+_C.data = 'data/coco128.yaml'  #'data.yaml path')
+_C.hyp = 'data/hyp.scratch.yaml'  #'hyperparameters path')
+_C.epochs = 300
+_C.batch_size = 16  #'total batch size for all GPUs')
+_C.img_size = [640, 640]  #'[train, test] image sizes')
+_C.rect = True  #'rectangular training')
+_C.resume = False  #'resume most recent training')
+_C.nosave = True  #'only save final checkpoint')
+_C.notest = True  #'only test final epoch')
+_C.noautoanchor = True  #'disable autoanchor check')
+_C.evolve = True  #'evolve hyperparameters')
+_C.bucket = ''  #'gsutil bucket')
+_C.cache_images = True  #'cache images for faster training')
+_C.image_weights = True  #'use weighted image selection for training')
+_C.device = ''  #'cuda device, i.e. 0 or 0,1,2,3 or cpu')
+_C.multi_scale = True  #'vary img_size +/- 50%%')
+_C.single_cls = True  #'train multi_class data as single_class')
+_C.adam = True  #'use torch.optim.Adam() optimizer')
+_C.sync_bn = True  #'use SyncBatchNorm, only available in DDP mode')
+_C.local_rank = -1  #'DDP parameter, do not modify')
+_C.log_imgs = 16  #'number of images for W&B logging, max 100')
+_C.log_artifacts = True  #'log artifacts, i.e. final trained model')
+_C.workers = 8  #'maximum number of dataloader workers')
+_C.project = 'runs/train'  #'save to project/name')
+_C.name = 'exp'  #'save to project/name')
+_C.exist_ok = True  #'existing project/name ok, do not increment')
+_C.quad = True  #'quad dataloader')
+
+_C.SYSTEM = CN()
+
+def get_C_defaults():
+  """Get a yacs CNode object with default values for my_project."""
+  # Return a clone so that the defaults will not be altered
+  # This is for the "local variable" use pattern
+  C = _C.clone()
+
+  C.data, C.C, C.hyp = check_file(C.data), check_file(C.C), check_file(C.hyp)  # check files
+
+  # Set DDP variables
+  C.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
+  C.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1
+
+  C.img_size.extend([C.img_size[-1]] * (2 - len(C.img_size)))  # extend to 2 sizes (train, test)
+  C.name = 'evolve' if C.evolve else C.name
+  C.save_dir = increment_path(Path(C.project) / C.name, exist_ok=C.exist_ok | C.evolve)  # increment run
+
+  C.total_batch_size = C.batch_size
+  assert C.batch_size % C.world_size == 0, '--batch-size must be multiple of CUDA device count'
+  C.batch_size = C.total_batch_size // C.world_size
+
+  return C
--- train.py
+++ train.py
@@ -33,6 +33,8 @@ from utils.loss import ComputeLoss
 from utils.plots import plot_images, plot_labels, plot_results, plot_evolution
 from utils.torch_utils import ModelEMA, select_device, intersect_dicts, torch_distributed_zero_first
 
+from data.config_default import get_cfg_defaults
+
 logger = logging.getLogger(__name__)
 
 
@@ -469,100 +471,74 @@ def train(hyp, opt, device, tb_writer=None, wandb=None):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', type=str, default='yolov5s.pt', help='initial weights path')
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--data', type=str, default='data/coco128.yaml', help='data.yaml path')
-    parser.add_argument('--hyp', type=str, default='data/hyp.scratch.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
-    parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
-    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
-    parser.add_argument('--notest', action='store_true', help='only test final epoch')
-    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
-    parser.add_argument('--evolve', action='store_true', help='evolve hyperparameters')
-    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
-    parser.add_argument('--cache-images', action='store_true', help='cache images for faster training')
-    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
-    parser.add_argument('--single-cls', action='store_true', help='train multi-class data as single-class')
-    parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
-    parser.add_argument('--log-imgs', type=int, default=16, help='number of images for W&B logging, max 100')
-    parser.add_argument('--log-artifacts', action='store_true', help='log artifacts, i.e. final trained model')
-    parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
-    parser.add_argument('--project', default='runs/train', help='save to project/name')
-    parser.add_argument('--name', default='exp', help='save to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--quad', action='store_true', help='quad dataloader')
-    opt = parser.parse_args()
+    parser = argparse.ArgumentParser(description="PyTorch DeeplabV3Plus Training")
+    parser.add_argument('config_file', help='config file path')
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
 
-    # Set DDP variables
-    opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
-    opt.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1
-    set_logging(opt.global_rank)
-    if opt.global_rank in [-1, 0]:
+    args = parser.parse_args()
+
+    cfg = get_cfg_defaults()
+    cfg.merge_from_file(args.config_file)
+    cfg.merge_from_list(args.opts)
+    print(cfg)
+    
+    set_logging(cfg.global_rank)
+    if cfg.global_rank in [-1, 0]:
         check_git_status()
         check_requirements()
 
     # Resume
-    if opt.resume:  # resume an interrupted run
-        ckpt = opt.resume if isinstance(opt.resume, str) else get_latest_run()  # specified or most recent path
+    if cfg.resume:  # resume an interrupted run
+        ckpt = cfg.resume if isinstance(cfg.resume, str) else get_latest_run()  # specified or most recent path
         assert os.path.isfile(ckpt), 'ERROR: --resume checkpoint does not exist'
-        apriori = opt.global_rank, opt.local_rank
-        with open(Path(ckpt).parent.parent / 'opt.yaml') as f:
-            opt = argparse.Namespace(**yaml.load(f, Loader=yaml.SafeLoader))  # replace
-        opt.cfg, opt.weights, opt.resume, opt.batch_size, opt.global_rank, opt.local_rank = '', ckpt, True, opt.total_batch_size, *apriori  # reinstate
+        apriori = cfg.global_rank, cfg.local_rank
+        with open(Path(ckpt).parent.parent / 'cfg.yaml') as f:
+            cfg = argparse.Namespace(**yaml.load(f, Loader=yaml.SafeLoader))  # replace
+        cfg.cfg, cfg.weights, cfg.resume, cfg.batch_size, cfg.global_rank, cfg.local_rank = '', ckpt, True, cfg.total_batch_size, *apriori  # reinstate
         logger.info('Resuming training from %s' % ckpt)
     else:
-        # opt.hyp = opt.hyp or ('hyp.finetune.yaml' if opt.weights else 'hyp.scratch.yaml')
-        opt.data, opt.cfg, opt.hyp = check_file(opt.data), check_file(opt.cfg), check_file(opt.hyp)  # check files
-        assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
-        opt.img_size.extend([opt.img_size[-1]] * (2 - len(opt.img_size)))  # extend to 2 sizes (train, test)
-        opt.name = 'evolve' if opt.evolve else opt.name
-        opt.save_dir = increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok | opt.evolve)  # increment run
+        # cfg.hyp = cfg.hyp or ('hyp.finetune.yaml' if cfg.weights else 'hyp.scratch.yaml')
+        assert len(cfg.cfg) or len(cfg.weights), 'either --cfg or --weights must be specified'
 
     # DDP mode
-    opt.total_batch_size = opt.batch_size
-    device = select_device(opt.device, batch_size=opt.batch_size)
-    if opt.local_rank != -1:
-        assert torch.cuda.device_count() > opt.local_rank
-        torch.cuda.set_device(opt.local_rank)
-        device = torch.device('cuda', opt.local_rank)
+    device = select_device(cfg.device, batch_size=cfg.batch_size)
+    if cfg.local_rank != -1:
+        assert torch.cuda.device_count() > cfg.local_rank
+        torch.cuda.set_device(cfg.local_rank)
+        device = torch.device('cuda', cfg.local_rank)
         dist.init_process_group(backend='nccl', init_method='env://')  # distributed backend
-        assert opt.batch_size % opt.world_size == 0, '--batch-size must be multiple of CUDA device count'
-        opt.batch_size = opt.total_batch_size // opt.world_size
 
     # Hyperparameters
-    with open(opt.hyp) as f:
+    with open(cfg.hyp) as f:
         hyp = yaml.load(f, Loader=yaml.SafeLoader)  # load hyps
 
     # Train
-    logger.info(opt)
+    logger.info(cfg)
     try:
         import wandb
     except ImportError:
         wandb = None
         prefix = colorstr('wandb: ')
         logger.info(f"{prefix}Install Weights & Biases for YOLOv5 logging with 'pip install wandb' (recommended)")
-    if not opt.evolve:
+    if not cfg.evolve:
         tb_writer = None  # init loggers
-        if opt.global_rank in [-1, 0]:
-            logger.info(f'Start Tensorboard with "tensorboard --logdir {opt.project}", view at http://localhost:6006/')
-            tb_writer = SummaryWriter(opt.save_dir)  # Tensorboard
-        train(hyp, opt, device, tb_writer, wandb)
+        if cfg.global_rank in [-1, 0]:
+            logger.info(f'Start Tensorboard with "tensorboard --logdir {cfg.project}", view at http://localhost:6006/')
+            tb_writer = SummaryWriter(cfg.save_dir)  # Tensorboard
+        train(hyp, cfg, device, tb_writer, wandb)
 
-    # Evolve hyperparameters (optional)
+    # Evolve hyperparameters (cfgional)
     else:
         # Hyperparameter evolution metadata (mutation scale 0-1, lower_limit, upper_limit)
         meta = {'lr0': (1, 1e-5, 1e-1),  # initial learning rate (SGD=1E-2, Adam=1E-3)
                 'lrf': (1, 0.01, 1.0),  # final OneCycleLR learning rate (lr0 * lrf)
                 'momentum': (0.3, 0.6, 0.98),  # SGD momentum/Adam beta1
-                'weight_decay': (1, 0.0, 0.001),  # optimizer weight decay
+                'weight_decay': (1, 0.0, 0.001),  # cfgimizer weight decay
                 'warmup_epochs': (1, 0.0, 5.0),  # warmup epochs (fractions ok)
                 'warmup_momentum': (1, 0.0, 0.95),  # warmup initial momentum
                 'warmup_bias_lr': (1, 0.0, 0.2),  # warmup initial bias lr
@@ -588,12 +564,12 @@ if __name__ == '__main__':
                 'mosaic': (1, 0.0, 1.0),  # image mixup (probability)
                 'mixup': (1, 0.0, 1.0)}  # image mixup (probability)
 
-        assert opt.local_rank == -1, 'DDP mode not implemented for --evolve'
-        opt.notest, opt.nosave = True, True  # only test/save final epoch
+        assert cfg.local_rank == -1, 'DDP mode not implemented for --evolve'
+        cfg.notest, cfg.nosave = True, True  # only test/save final epoch
         # ei = [isinstance(x, (int, float)) for x in hyp.values()]  # evolvable indices
-        yaml_file = Path(opt.save_dir) / 'hyp_evolved.yaml'  # save best result here
-        if opt.bucket:
-            os.system('gsutil cp gs://%s/evolve.txt .' % opt.bucket)  # download evolve.txt if exists
+        yaml_file = Path(cfg.save_dir) / 'hyp_evolved.yaml'  # save best result here
+        if cfg.bucket:
+            os.system('gsutil cp gs://%s/evolve.txt .' % cfg.bucket)  # download evolve.txt if exists
 
         for _ in range(300):  # generations to evolve
             if Path('evolve.txt').exists():  # if evolve.txt exists: select best hyps and mutate
@@ -628,10 +604,10 @@ if __name__ == '__main__':
                 hyp[k] = round(hyp[k], 5)  # significant digits
 
             # Train mutation
-            results = train(hyp.copy(), opt, device, wandb=wandb)
+            results = train(hyp.copy(), cfg, device, wandb=wandb)
 
             # Write mutation results
-            print_mutation(hyp.copy(), results, yaml_file, opt.bucket)
+            print_mutation(hyp.copy(), results, yaml_file, cfg.bucket)
 
         # Plot results
         plot_evolution(yaml_file)
